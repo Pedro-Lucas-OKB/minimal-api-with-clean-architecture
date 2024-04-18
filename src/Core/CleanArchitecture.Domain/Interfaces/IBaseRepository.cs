e5479569c2408b1c6c5b593d64cc5680d293fa4a
Pedro Lucas <pedrogmobile2@gmail.com>
2024-04-18 17:33:13 -0300

    Implemented domain layer

---
+++ src/Core/CleanArchitecture.Domain/Interfaces/IBaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Domain.Interfaces;
+
+public interface IBaseRepository<T> where T : BaseEntity
+{
+    Task CreateAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+
+    Task<T> GetAsync(Guid id, CancellationToken cancellationToken);
+    Task<List<T>> GetAllAsync(Guid id, CancellationToken cancellationToken);
+}

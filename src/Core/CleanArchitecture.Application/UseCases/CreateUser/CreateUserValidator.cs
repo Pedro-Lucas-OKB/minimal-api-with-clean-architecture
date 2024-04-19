630a2a71026c329c01cfd6c26ee9ef24b4b7524e
Pedro Lucas <pedrogmobile2@gmail.com>
2024-04-18 23:54:03 -0300

    added automapper, fluentvalidation and mediatr packages. also, implemented CreateUser use case

---
+++ src/Core/CleanArchitecture.Application/UseCases/CreateUser/CreateUserValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace CleanArchitecture.Application.UseCases.CreateUser;
+
+public sealed class CreateUserValidator : AbstractValidator<CreateUserRequest>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(user => user.Email).NotEmpty().MaximumLength(64).EmailAddress();
+        RuleFor(user => user.Name).NotEmpty().MinimumLength(3).MaximumLength(75);
+    }
+}
